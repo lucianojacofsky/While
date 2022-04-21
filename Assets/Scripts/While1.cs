02cb5998bff5a305d962dd51727628e4dba8483d
lucianojacofsky <101577485+lucianojacofsky@users.noreply.github.com>
2022-04-21 08:59:06 -0300

    Finish

---
+++ Assets/Scripts/While1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class While1 : MonoBehaviour
+{
+    public int counter, counterMax;
+    public Text txt_display;
+    // Start is called before the first frame update
+    void Start()
+    {
+        while(counter<counterMax)
+        {
+            Debug.Log("repetición");
+            counter++;
+        }
+        txt_display.text = "Terminó el while";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
